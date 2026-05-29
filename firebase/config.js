1e5d33550cedc91013b368db82bdc161e01b7765
Rusetska Tetiana <rusetskatata@gmail.com>
2026-05-29 17:40:12 +0200

    install firebase

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+export const auth = getAuth(app);
+export default app;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADxi0Zp6pHMJxhyW08hZFBoHq4Zi2MYIM",
+  authDomain: "fotoarchiv--mono.firebaseapp.com",
+  projectId: "fotoarchiv--mono",
+  appId: "1:712914217790:web:45e9614178b6088217da6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
